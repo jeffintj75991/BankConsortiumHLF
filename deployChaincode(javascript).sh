5624df0a6cfd9079d1a0b380b4fe81745a362ce6
Jeffin T.J <jeff.jo95z@gmail.com>
2022-03-16 14:25:04 +0530

    Add files via upload

---
+++ deployChaincode(javascript).sh
@@ -0,0 +1,185 @@
+export CORE_PEER_TLS_ENABLED=true
+export ORDERER_CA=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export PEER0_ORG1_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export FABRIC_CFG_PATH=${PWD}/artifacts/channel/config/
+
+export CHANNEL_NAME=mychannel
+
+setGlobalsForOrderer() {
+    export CORE_PEER_LOCALMSPID="OrdererMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/users/Admin@example.com/msp
+
+}
+
+setGlobalsForPeer0Org1() {
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+}
+
+setGlobalsForOrg1() {
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+}
+
+
+
+
+# presetup
+
+CHANNEL_NAME="mychannel"
+CC_RUNTIME_LANGUAGE="node"
+VERSION="1"
+SEQUENCE="1"
+CC_SRC_PATH="./artifacts/src/github.com/banktrxn/"
+CC_NAME="banktrax"
+
+packageChaincode() {
+    rm -rf ${CC_NAME}.tar.gz
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode package ${CC_NAME}.tar.gz \
+        --path ${CC_SRC_PATH} --lang ${CC_RUNTIME_LANGUAGE} \
+        --label ${CC_NAME}_${VERSION}
+    echo "===================== Chaincode is packaged ===================== "
+}
+# packageChaincode
+
+installChaincode() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode install ${CC_NAME}.tar.gz
+    echo "===================== Chaincode is installed on peer0.org1 ===================== "
+}
+
+# installChaincode
+
+queryInstalled() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode queryinstalled >&log.txt
+    cat log.txt
+    PACKAGE_ID=$(sed -n "/${CC_NAME}_${VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
+    echo PackageID is ${PACKAGE_ID}
+    echo "===================== Query installed successful on peer0.org1 on channel ===================== "
+}
+
+approveForMyOrg1() {
+    setGlobalsForPeer0Org1
+    # set -x
+    peer lifecycle chaincode approveformyorg -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.example.com --tls \
+        --cafile $ORDERER_CA --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
+        --init-required --package-id ${PACKAGE_ID} \
+        --sequence ${SEQUENCE}
+    # set +x
+
+    echo "===================== chaincode approved from org 1 ===================== "
+
+}
+
+checkCommitReadyness() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode checkcommitreadiness \
+        --channelID $CHANNEL_NAME --name ${CC_NAME} --version ${VERSION} \
+        --sequence ${VERSION} --output json --init-required
+    echo "===================== checking commit readyness from org 1 ===================== "
+}
+
+# checkCommitReadyness
+
+
+# checkCommitReadyness
+
+commitChaincodeDefination() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com \
+        --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
+        --channelID $CHANNEL_NAME --name ${CC_NAME} \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        --version ${VERSION} --sequence ${SEQUENCE} --init-required
+
+ echo "===================== commit Chaincode Defination ===================== "
+}
+
+# commitChaincodeDefination
+
+queryCommitted() {
+    setGlobalsForPeer0Org1
+    peer lifecycle chaincode querycommitted --channelID $CHANNEL_NAME --name ${CC_NAME}
+
+}
+
+# queryCommitted
+
+chaincodeInvokeInit() {
+    setGlobalsForPeer0Org1
+    peer chaincode invoke -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.example.com \
+        --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA \
+        -C $CHANNEL_NAME -n ${CC_NAME} \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        --isInit -c '{"Args":[]}'
+
+}
+
+# chaincodeInvokeInit
+
+chaincodeInvoke() {
+    setGlobalsForPeer0Org1
+
+
+    peer chaincode invoke -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.example.com \
+        --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA \
+        -C $CHANNEL_NAME -n ${CC_NAME}  \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        -c '{"function":"createBankTrxns","Args":["{\"transactionId\":\"TXN00000003\",\"country\":\"Indonesia\",\"currency\":\"Rupiah\",\"amount\":\"200000\", \"origin\":\"US\"}"]}'
+
+}
+
+
+chaincodeInvoke2() {
+    setGlobalsForPeer0Org1
+
+ 
+    peer chaincode invoke -o localhost:7050 \
+        --ordererTLSHostnameOverride orderer.example.com \
+        --tls $CORE_PEER_TLS_ENABLED \
+        --cafile $ORDERER_CA \
+        -C $CHANNEL_NAME -n ${CC_NAME}  \
+        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
+        -c '{"function":"changeBankTrxns","Args":["TXN00000003","100"]}'
+
+}
+
+chaincodeQuery() {
+    setGlobalsForPeer0Org1
+    # setGlobalsForOrg1
+    peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"function": "queryBankTrxns","Args":["TXN00000003"]}'
+ 
+   echo "===================== Asset history ===================== "
+    peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"Args":["GetAssetHistory","TXN00000003"]}'
+}
+
+# chaincodeQuery
+
+# Run this function if you add any new dependency in chaincode
+
+
+packageChaincode
+installChaincode
+queryInstalled
+approveForMyOrg1
+checkCommitReadyness
+commitChaincodeDefination
+queryCommitted
+
+chaincodeInvokeInit
+sleep 5
+ chaincodeInvoke
+chaincodeInvoke2
+   sleep 3
+chaincodeQuery
